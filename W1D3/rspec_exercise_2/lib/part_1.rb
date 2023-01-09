0d356a15c27e0e951c2f3087879c23fceff2c580
Tysuiku <Timothydong41@gmail.com>
2023-01-09 18:00:44 -0500

    working on day 3 work

---
+++ W1D3/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,40 @@
+def partition(arr, num)
+  result = []
+  result << arr.select { |ele| ele < num }
+  result << arr.select { |ele| ele >= num }
+  result
+end
+
+def merge(hash_1, hash_2)
+  hash = {}
+  hash_1.each { |k, v| hash[k] = v }
+  hash_2.each { |k, v| hash[k] = v }
+  hash
+end
+
+def censor(sentence, curses)
+  new_arr = []
+  words = sentence.split(" ")
+  words.each do |word|
+    if curses.include? word.downcase
+      new_arr << suisei(word)
+    else
+      new_arr << word
+    end
+  end
+
+  new_arr.join(" ")
+end
+
+def suisei(word)
+  str = ""
+  word.each_char do |i|
+    if "aeiou".include? i.downcase
+      str += "*"
+    else
+      str += i
+    end
+  end
+
+  str
+end
