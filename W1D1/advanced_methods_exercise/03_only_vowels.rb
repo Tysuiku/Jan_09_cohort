63b993e6f72ef3f8a6a105845f44eae1e968404e
Tysuiku <Timothydong41@gmail.com>
2023-01-09 11:03:41 -0500

    advanced methods done

--- W1D1/advanced_methods_exercise/03_only_vowels.rb
+++ W1D1/advanced_methods_exercise/03_only_vowels.rb
@@ -2,10 +2,11 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+def only_vowels?(str)
+  str.split("").all? { |char| "aeiou".include? char }
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
 p only_vowels?("cat")     # => false
 p only_vowels?("over")    # => false
-
-
