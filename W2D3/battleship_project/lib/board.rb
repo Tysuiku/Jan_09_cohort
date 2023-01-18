38d053c3e7b7de6904a4a75572d4ba6d758060b3
Tysuiku <Timothydong41@gmail.com>
2023-01-18 10:09:28 -0500

    worked on w2d3 work yesterday

--- W2D3/battleship_project/lib/board.rb
+++ W2D3/battleship_project/lib/board.rb
@@ -1,3 +1,64 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @grid[pos[0]][pos[1]] = value
+  end
+
+  def num_ships
+    count = 0
+    @grid.each do |row|
+      row.each do |ele|
+        if ele == :S
+          count += 1
+        end
+      end
+    end
+
+    count
+  end
+
+  def attack(pos)
+    if self[pos] == :S
+      self[pos] = :H
+      p "you sunk my battleship!"
+      return true
+    else
+      self[pos] = :X
+    end
+
+    false
+  end
+
+  def place_random_ships
+    total_ships = @size * 0.25
+
+    while self.num_ships < total_ships
+      row = rand(0...@grid.length)
+      col = rand(0...@grid.length)
+      pos = [row, col]
+      self[pos] = :S
+    end
+  end
+
+  def hidden_ships_grid
+    @grid.map do |row|
+      row.map do |ele|
+        if ele == :S
+          :N
+        else
+          ele
+        end
+      end
+    end
+  end
 end
