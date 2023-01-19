21162753d36e84bae89f2c337d388bb88a728269
Tysuiku <Timothydong41@gmail.com>
2023-01-19 11:40:17 -0500

    added work

--- W2D3/assessment_2b/lib/board.rb
+++ W2D3/assessment_2b/lib/board.rb
@@ -1,9 +1,16 @@
 class Board
+  def build_stacks(n)
+    Array.new(n) { Array.new }
+  end
 
+  def initialize(stacks, max_height)
+    @max_height = max_height
+    @stacks = Array.new(max_height)
+  end
 
-    # This Board#print method is given for free and does not need to be modified
-    # It is used to make your game playable.
-    def print
-        @stacks.each { |stack| p stack }
-    end
+  # This Board#print method is given for free and does not need to be modified
+  # It is used to make your game playable.
+  def print
+    @stacks.each { |stack| p stack }
+  end
 end
