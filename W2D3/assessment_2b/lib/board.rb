ae0372f08c83bd0eadae9a54e04391795eefc523
Tysuiku <Timothydong41@gmail.com>
2023-01-20 18:24:01 -0500

    added work

--- W2D3/assessment_2b/lib/board.rb
+++ W2D3/assessment_2b/lib/board.rb
@@ -6,8 +6,35 @@ class Board
   attr_reader :max_height
 
   def initialize(stacks, max_height)
+    raise ArgumentError.new "rows and cols must be >= 4" unless max_height > 4 && stacks > 4
     @max_height = max_height
-    @stacks = Array.new(stacks) { Array.new }
+    @stacks = self.class.build_stacks(stacks)
+  end
+
+  def add(token, idx)
+    if @stacks[idx].length < @max_height
+      @stacks[idx] << token
+      return true
+    end
+    false
+  end
+
+  def vertical_winner?(token)
+    @stacks.any? do |arr|
+      arr.length == max_height && arr.all? { |ele| ele == token }
+    end
+  end
+
+  def horizontal_winner?(token)
+    (0...@max_height).any? do |level_idx|
+      @stacks.each_index.all? do |stack_idx|
+        @stacks[stack_idx][level_idx] == token
+      end
+    end
+  end
+
+  def winner?(token)
+    horizontal_winner?(token) || vertical_winner?(token)
   end
 
   # This Board#print method is given for free and does not need to be modified
